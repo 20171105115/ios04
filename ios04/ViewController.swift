01d77b8833e3fcf16620e54d9335672e94541c2f
朱博宇 <s20171105115@chu1e1adeimac.local>
2018-09-21 09:35:54 +0800

    Initial Commit

---
+++ ios04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios04
+//
+//  Created by 朱博宇 on 2018/9/21.
+//  Copyright © 2018年 朱博宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios04Tests/ios04Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios04Tests.swift
+//  ios04Tests
+//
+//  Created by 朱博宇 on 2018/9/21.
+//  Copyright © 2018年 朱博宇. All rights reserved.
+//
+
+import XCTest
+@testable import ios04
+
+class ios04Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
