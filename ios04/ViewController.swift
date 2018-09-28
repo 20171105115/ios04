77f0c17fbfdda7259ff17d94b1f8f719cff6409b
朱博宇 <s20171105115@chu1e1adeiMac.local>
2018-09-28 09:35:29 +0800

    基本实现计算机功能
    实现了计算器的加减乘除，平方开根之类的功能也可以增加，但是遇到括号之类的还需要堆栈之类的知识，有待改进。。。

--- ios04/ViewController.swift
+++ ios04/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var temp = 0
+    var flag = 0
     @IBOutlet weak var number: UITextField!
     
     @IBAction func Num1(_ sender: Any) {
@@ -22,13 +23,13 @@ class ViewController: UIViewController {
     @IBAction func Num3(_ sender: Any) {
         number.text = number.text!+"3"
     }
-    @IBAction func Num4(_ sender: Any) {
+    @IBAction func Sum4(_ sender: Any) {
         number.text = number.text!+"4"
     }
-    @IBAction func Num5(_ sender: Any) {
+    @IBAction func Sum5(_ sender: Any) {
         number.text = number.text!+"5"
     }
-    @IBAction func Num6(_ sender: Any) {
+    @IBAction func Sum6(_ sender: Any) {
         number.text = number.text!+"6"
     }
     @IBAction func Num7(_ sender: Any) {
@@ -40,20 +41,60 @@ class ViewController: UIViewController {
     @IBAction func Num9(_ sender: Any) {
         number.text = number.text!+"9"
     }
+    @IBAction func Num0(_ sender: Any) {
+        number.text = number.text!+"0"
+    }
     @IBAction func equal(_ sender: Any) {
         var sum = 0
-        sum = temp + Int(number.text!)!
-        number.text = "\(sum)"
+        switch flag {
+        case 1:
+            sum = temp + Int(number.text!)!
+            number.text = "\(sum)"
+            
+        case 2:
+            sum = temp - Int(number.text!)!
+            number.text = "\(sum)"
+            
+        case 3:
+            sum = temp * Int(number.text!)!
+            number.text = "\(sum)"
+            
+        case 4:
+            if number.text=="0"
+            {
+                break
+            }else{
+                sum = temp / Int(number.text!)!
+                number.text = "\(sum)"
+            }
+            
+        default:
+            break
+        }
+    }
+    @IBAction func subtract(_ sender: Any){
+        flag = 2
+        temp = Int(number.text!)!
+        number.text = ""
+    }
+    @IBAction func ride(_ sender: Any) {
+        flag = 3
+        temp = Int(number.text!)!
+        number.text = ""
     }
     @IBAction func puls(_ sender: Any) {
+        flag = 1
+        temp = Int(number.text!)!
+        number.text = ""
+    }
+    @IBAction func divide(_ sender: Any) {
+        flag = 4
         temp = Int(number.text!)!
         number.text = ""
     }
     @IBAction func Delete(_ sender: Any) {
         number.text = ""
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
